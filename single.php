9a33a4763f4e82498b8c93c39216872bddc01994
伊東和成 <itokazunari5375@icloud.com>
2022-02-20 19:24:05 +0900

    アイキャッチを指定

--- single.php
+++ single.php
@@ -15,7 +15,15 @@
     <?php while (have_posts()) : the_post() ?>
 
       <!-- Page Header -->
-      <header class="masthead" style="background-image: url('img/post-bg.jpg')">
+      <!-- アイキャッチ画像の有無をチェック -->
+      <?php if (has_post_thumbnail()) :
+        $id = get_post_thumbnail_id();
+        $img = wp_get_attachment_image_src($id, "large");
+      else :
+        $img = array(get_template_directory_uri() . "/img/post-bg.jpg");
+      endif;
+      ?>
+      <header class="masthead" style="background-image: url('<?php echo $img[0] ?>')">
         <div class="overlay"></div>
         <div class="container">
           <div class="row">
@@ -36,6 +44,8 @@
         <div class="container">
           <div class="row">
             <div class="col-lg-8 col-md-10 mx-auto">
+              <?php the_post_thumbnail(array(32, 32), array("alt" => "アイキャッチ画像")); ?>
+
               <?php the_content(); ?>
             </div>
           </div>
